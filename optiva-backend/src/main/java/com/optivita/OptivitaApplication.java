9744fcb74483a8fe6428ec5a6305e108bc607bbe
Janidu Dhakshitha Yapa <janidudhakshi222@gmail.com>
2026-02-25 01:39:25 +0530

    Initial commit

---
+++ optiva-backend/src/main/java/com/optivita/OptivitaApplication.java
@@ -0,0 +1,13 @@
+package com.optivita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptivitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptivitaApplication.class, args);
+	}
+
+}
---
+++ optiva-backend/src/test/java/com/optivita/OptivitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optivita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptivitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
