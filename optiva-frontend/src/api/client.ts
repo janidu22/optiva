a13ebbbdbc3021d1d6cce7e628a14c890a712cb4
Janidu Dhakshitha Yapa <janidudhakshi222@gmail.com>
2026-02-26 01:29:13 +0530

    configured app to productions

--- optiva-frontend/src/api/client.ts
+++ optiva-frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { AuthResponse, RefreshTokenRequest } from "@/types";
 
-const API_BASE_URL = "http://localhost:5000/api/v1";
+const API_BASE_URL = "http://localhost:8080/api/v1";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
